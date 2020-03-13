1748b3110e17eaa597ffed68c4c69d17da8ef3c8
Ludovic Cointrel <ludo@iMac-de-mediactive-digital.local>
2020-03-13 17:21:09 +0100

    Avancée

--- libs/core.js
+++ libs/core.js
@@ -1,7 +1,13 @@
 const helpers = require('./helpers');
+
+console.log('helpers', helpers)
 class Wpk_Core {
     constructor(opts) {
         this.options = helpers.getConfig(opts);
+        console.log('this', this)
+    }
+    start() {
+        
     }
 }
 module.exports = Wpk_Core;
--- libs/helpers.js
+++ libs/helpers.js
@@ -1,9 +1,28 @@
 const fs = require('fs');
+const merge = require('deepmerge');
+
 class Wpk_Helpers {
-    getConfig() {
-        console.log('this ?', this);
-    }
-    mergeConfig() {
+    static getConfig(opts) {
+        console.log('opts', opts);
+
+        if(opts === undefined) {
+            opts = {
+                recursive: true,
+                mode: 'discover',
+                folders: 'resources',
+                mix: 'default',
+                plugins: []            
+            }
+            opts._mixConfig = require('../configs/default');
+        }
+        else {
+            opts = merge( require('../config.json'),  opts);
+            opts._mixConfig = require('../configs/'+ opts.mix +'');
+        }
+
+        // return merge( require('../configs/default') ,  opts);
+
+        return opts;
 
     }
 }
