da5963b06de6b830362d2b9e7bc91e6aae31eed6
l.cointrel <l.cointrel@mediactive.fr>
2020-03-14 10:38:02 +0100

    Avancée

--- libs/core.js
+++ libs/core.js
@@ -4,10 +4,39 @@ console.log('helpers', helpers)
 class Wpk_Core {
     constructor(opts) {
         this.options = helpers.getConfig(opts);
-        console.log('this', this)
+    }
+    getPlugins() {
+        return this.options.plugins;
+    }
+    getMode() {
+        return this.options.mode;
+    }
+    getFolders() {
+        return this.options.folders;
+    }
+    prepare() {
+        let paths = undefined;
+        let mode = this.getMode();
+        let folders = this.getFolders();
+        let recursive = this.options.recursive;
+        switch (mode) {
+            case "manual":
+
+                break;
+
+            case "discover":
+
+                break;
+
+            default:
+                console.log('The mode "'+mode+'" is undefined.'.red)
+                console.log('Modes availables are discover and manual'.underline.red)
+                process.exit(1);
+                break;
+        }
     }
     start() {
-        
+        this.prepare();
     }
 }
 module.exports = Wpk_Core;
--- libs/helpers.js
+++ libs/helpers.js
@@ -25,5 +25,31 @@ class Wpk_Helpers {
         return opts;
 
     }
+    static walker(dir, filelist, recursive) {
+        var fs = fs || require('fs'),
+        files = fs.existsSync(dir) ? fs.readdirSync(dir) : [],
+        filelist = filelist || [];
+        let that = this;
+        files.forEach(function(file) {
+            if(recursive != undefined && recursive) {
+                if (fs.statSync(path.join(dir, file)).isDirectory()) {
+                    filelist = that.walker(path.join(dir, file), filelist, true);
+                }
+                else {
+                    filelist.push(path.join(dir, file));
+                }
+            }
+            else {
+                var full_path = path.join(dir, file);
+                if(that.isFile(full_path) === true) {
+                    filelist.push(path.join(dir, file));
+                }
+
+            }
+
+        });
+        //  console.log('debug folder recursive', filelist)
+        return filelist;
+      }
 }
 module.exports = Wpk_Helpers;
