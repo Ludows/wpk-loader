42fb94a9b7c082db49f19935c77be53943b6c8b5
l.cointrel <l.cointrel@mediactive.fr>
2020-03-19 12:07:41 +0100

    A voir si la génération de liens symbloqies est astucieux ou non ( pas convaincu )

---
+++ postinstall.js
@@ -0,0 +1,5 @@
+const basePath = process.cwd();
+const path = require('path');
+const fs = require('fs');
+
+// a voir si la génération de liens symboliques pour appeler les plugins est judicieuse..
